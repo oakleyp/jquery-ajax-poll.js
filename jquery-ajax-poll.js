dc94399b562e465ef98eef0e2ec9b381f322b663
oakley <oakleypeavler@gmail.com>
2019-08-11 19:33:34 -0400

    add docs

--- jquery-ajax-poll.js
+++ jquery-ajax-poll.js
@@ -1,31 +1,50 @@
-function ajaxPoll({ajax, attempt, max_attempts, interval, until, timeout, finished}) {
-    const ajax_inst = ajax();
+/**
+ * @function ajaxPoll
+ * 
+ * A function that takes the given ajax() function and calls until() with the result until
+ * either the until() function returns a value interpreted as true, where finished() is called with the latest
+ * result; or the attempt exceeds the maximum number of attempts given, in which case timeout() is called 
+ * with the latest result
+ * 
+ * @param {object} args
+ * @param {function} args.ajax - a function that returns the result of calling $.ajax() 
+ * @param {number} args.attempt - the current attempt, defaults to 0
+ * @param {integer} args.maxAttempts - the maximum number of times to call the given ajax function 
+ * @param {number} args.interval - the interval in ms to call the passed ajax function
+ * @param {function} args.until - a function that, given the result of calling the passed ajax function, returns true when polling is finished
+ * @param {function} args.timeout - a function that is called when the current attempt exceed the maximum number of attempts
+ * @param {function} args.finished - a function that is called when the passed until callback is true; ie. the polling is complete
+ * 
+ * @returns {object} - an object with the following functions, where each accepts a callback that is called with the result of the latest ajax() call
+ * @returns {function} until - chainable function that accepts a callback where, true is returned, polling is finished, and the finished() function is called
+ * @returns {function} timeout - function that accepts a callback that is called if attempt exceeds the number of maxAttempts specified
+ * @returns {function} finished - function that accepts a callback that is called when the given until() function returns a "true" value
+ */
+function ajaxPoll({ajax, attempt, maxAttempts = 60, interval = 8000, until, timeout, finished}) {
     attempt = (attempt && ++attempt) || 1;
-    max_attempts = max_attempts || 60;
-    interval = interval || 8000;
 
-    if (typeof ajax === 'function' && typeof until === 'function' && typeof finished === 'function') {
-        const _ajax_callback = (...ajax_args) => { 
-            if (!until(...ajax_args) && attempt <= max_attempts) {
-                return setTimeout(() => ajaxPoll({ajax, attempt, max_attempts, interval, until, timeout, finished}), interval);
-            } else if (attempt > max_attempts && typeof timeout === 'function') {
-                return timeout(...ajax_args); 
+    if (typeof until === 'function' && typeof finished === 'function') {
+        const _ajaxCallback = (...ajaxArgs) => { 
+            if (!until(...ajaxArgs) && attempt < maxAttempts) {
+                return setTimeout(() => ajaxPoll({ajax, attempt, maxAttempts, interval, until, timeout, finished}), interval);
+            } else if (attempt >= maxAttempts && typeof timeout === 'function') {
+                return timeout(...ajaxArgs);
             } else {
-                return finished(...ajax_args);
+                return finished(...ajaxArgs);
             }
         };
 
-        ajax_inst.always(_ajax_callback);
+        ajax().always(_ajaxCallback);
     }
 
-    return Object.assign(ajax_inst, {
+    return {
         until(fn) {
-            const until_chain_callback = (typeof until === 'function') ? // If there's already an until function, create a new until function that combines it and the next caller's
-                (...ajax_args) => { return until(...ajax_args) && fn(...ajax_args) } :
+            const unitlChainCallback = (typeof until === 'function') ? // If there's already an until function, create a new until function that combines it and the next caller's
+                (...ajaxArgs) => { return until(...ajaxArgs) && fn(...ajaxArgs) } :
                 fn;
-            return ajaxPoll({ajax, attempt, max_attempts, interval, until: until_chain_callback, timeout, finished});
+            return ajaxPoll({ajax, attempt, maxAttempts, interval, until: unitlChainCallback, timeout, finished});
         },
-        timeout: (fn) => ajaxPoll({ajax, attempt, max_attempts, interval, until, timeout: fn, finished}),
-        finished: (fn) => ajaxPoll({ajax, attempt: 0, max_attempts, interval, until, timeout, finished: fn})
-    });
+        timeout: (fn) => ajaxPoll({ajax, attempt, maxAttempts, interval, until, timeout: fn, finished}),
+        finished: (fn) => ajaxPoll({ajax, attempt: 0, maxAttempts, interval, until, timeout, finished: fn})
+    };
 }
